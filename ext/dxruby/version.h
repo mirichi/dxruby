1e2cd1e0048e8293ca1899d144eb01de8ec89630
mirichi <sawara01@gmail.com>
2018-12-10 22:46:58 +0900

    1.4.6

--- ext/dxruby/version.h
+++ ext/dxruby/version.h
@@ -1,10 +1,10 @@
 /*
 ###################################
 #
-# DXRuby 1.4.5
+# DXRuby 1.4.6
 #
 ###################################
 */
-#define DXRUBY_VERSION "1.4.5"
+#define DXRUBY_VERSION "1.4.6"
 
 //#define DXRUBY15
